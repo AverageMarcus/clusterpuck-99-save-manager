456402b9a1198e6803ab18c6a5de34875bbafc3d
Marcus Noble <github@marcusnoble.co.uk>
2019-10-13 20:21:06 +0100

    Added level preview

--- script.js
+++ script.js
@@ -7,7 +7,8 @@ const vm = new Vue({
       saveFile: {},
       colors: [],
       users: [],
-      customLevels: []
+      customLevels: [],
+      mapShown: false
     }
   },
   methods: {
@@ -92,7 +93,7 @@ const vm = new Vue({
           const parts = level._value.split(';')
           const name = parts[1];
           const tagline = parts[2];
-          const plays = parts[3];
+          const backgroundColour = parts[3];
           const minPlayers = parts[4];
           const maxPlayers = parts[5];
           const levelID = parts[6];
@@ -101,9 +102,9 @@ const vm = new Vue({
           const levelData = parts.slice(9);
           const toRaw = function() {
             const level = this;
-            return ['70', level.name, level.tagline, level.plays, level.minPlayers, level.maxPlayers, level.levelID, level.difficulty, level.includeInList, ...level.levelData].join(';')
+            return ['70', level.name, level.tagline, level.backgroundColour, level.minPlayers, level.maxPlayers, level.levelID, level.difficulty, level.includeInList, ...level.levelData].join(';')
           }
-          return { name, tagline, plays, minPlayers, maxPlayers, levelID, difficulty, includeInList, levelData, toRaw };
+          return { name, tagline, backgroundColour, minPlayers, maxPlayers, levelID, difficulty, includeInList, levelData, toRaw };
         })
     },
     holdTime(time) {
@@ -129,7 +130,151 @@ const vm = new Vue({
       if (level) {
         downloadFile(level.name, level.toRaw());
       }
+    },
+    hideMap() {
+      this.mapShown = false;
+      const mapModal = document.querySelector('#mapModal');
+      mapModal.innerHTML = "";
+      const cell = document.createElement('div');
+      cell.classList.add('cell');
+      for (let i=0; i < (61 * 61); i++) {
+        mapModal.appendChild(cell.cloneNode(false));
+      }
+    },
+    showMap(parts, backgroundColour) {
+      let layout = parts[0].substring(355).split('');
+      let colours = parts[1].substring(346).split('');
+      let rotations = parts[2].substring(346).split('')
+
+      const MAP_WIDTH = 61;
+      const MAP_HEIGHT = 61;
+      const ROW_BUFFER = 9;
+      const ROW_TOTAL=MAP_WIDTH + ROW_BUFFER;
+      const MAP_CONTAINER='#mapModal';
+
+      document.querySelector(MAP_CONTAINER).style.backgroundColor = toBackgroundColour(backgroundColour);
+
+      for (let rowIndex = 0; rowIndex < MAP_HEIGHT; rowIndex++) {
+        let row = layout.splice(0, ROW_TOTAL)
+        row.forEach((cell, columnIndex) => {
+          let position = (rowIndex * MAP_WIDTH) + columnIndex + 1; // +1 because CSS starts arrays at 1
+          const colour = colours[(ROW_TOTAL * rowIndex) + ROW_TOTAL - 1 - columnIndex];
+          const rotation = rotations[(ROW_TOTAL * rowIndex) + ROW_TOTAL - 1 - columnIndex];
+          const cellElement = document.querySelector(MAP_CONTAINER + ' .cell:nth-of-type(' + position + ')');
+          if (cellElement) {
+            cellElement.dataset.row = rowIndex;
+            cellElement.dataset.columns = columnIndex;
+            cellElement.dataset.colourSymbol = colour;
+            cellElement.dataset.colour = toColour(colour);
+
+            cellElement.style.transform = "rotate(" + toDegs(rotation) + ")"
+
+            switch (cell) {
+              case '"':
+                cellElement.style.background = toColour(colour);
+                cellElement.style.border = "1px solid black";
+                break;
+              case "#":
+                cellElement.style.background = "repeating-linear-gradient(45deg, brown, brown 1px, grey 1px, grey 2px)";
+                break;
+              case "*":
+                cellElement.style.background = "red";
+                cellElement.style.borderRadius = "100%";
+                cellElement.innerText = "g"
+                break;
+              case "+":
+                cellElement.style.background = "blue";
+                cellElement.style.borderRadius = "100%";
+                cellElement.innerText = "g"
+                break;
+              case ",":
+                cellElement.style.background = "red";
+                cellElement.style.borderRadius = "100%";
+                cellElement.innerText = "s"
+                break;
+              case "-":
+                cellElement.style.background = "blue";
+                cellElement.style.borderRadius = "100%";
+                cellElement.innerText = "s"
+                break;
+              case "$":
+                cellElement.style.background = toColour(colour);
+                cellElement.style.border = "1px solid black";
+                cellElement.style.clipPath = "polygon(0% 0%, 100% 0%, 100% 100%)";
+                break;
+              case ")":
+                cellElement.style.background = "limegreen";
+                cellElement.style.borderRadius = "100%";
+                cellElement.innerText = "b"
+                break;
+              case "(":
+                cellElement.style.background = "orange";
+                cellElement.style.borderRadius = "100%";
+                cellElement.style.borderColor = "yello";
+                cellElement.innerText = "s"
+                break;
+              case "'":
+                cellElement.style.background = toColour(colour);
+                cellElement.style.border = "1px solid black";
+                cellElement.innerText = "*"
+                cellElement.style.fontSize = "25px";
+                cellElement.style.lineHeight = "22px";
+                break;
+            }
+          }
+        })
+      }
+
+      function toColour(colour) {
+        switch (colour) {
+          case '!': return "white"
+          case '"': return "gray"
+          case '#': return "darkgrey"
+          case '$': return "black"
+          case '%': return "red"
+          case '&': return "orange"
+          case "'": return "yellow"
+          case '(': return "green"
+          case ')': return "teal"
+          case '*': return "blue"
+          case '+': return "purple"
+          case ',': return "brown"
+          default: return "white"
+        }
+      }
+
+      function toBackgroundColour(colour) {
+        switch (colour) {
+          case '0': return "grey"
+          case '1': return "skyblue"
+          case '2': return "orange"
+          case '3': return "purple"
+          case '4': return "blue"
+          case '5': return "black"
+          case "6": return "green"
+          case '7': return "hotpink"
+          case '8': return "darkgreen"
+          case '9': return "beige"
+          case '10': return "red"
+          default: return "#424040"
+        }
+      }
+
+      function toDegs(rotation) {
+        switch (rotation) {
+          case '#': return "180deg";
+          case '"': return "90deg";
+          case '$': return "-90deg";
+          default: return "0deg";
+        }
+      }
+
+      this.mapShown = true;
+
     }
+  },
+  created() {
+    this.hideMap();
   }
 });
 
